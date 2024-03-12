91a872f14fb768748fab269a8a7883781178e949
Sami Khan <120458750+Samidatabeys@users.noreply.github.com>
2024-03-12 17:46:52 +0500

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
   setTimeout(function () {
     console.log("Howdy!");
-
     var divElement = document.getElementById("swell-customer-identification");
     var email = divElement.getAttribute("data-email");
     console.log(email);
     var iframe = document.getElementById("iframe");
     iframe.contentWindow.postMessage(email, "*");
-  }, 5000);
+  }, 10000);
 });
