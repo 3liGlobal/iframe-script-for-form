b24a4ed97c00bdaa80173caef66b51d7ff95729c
Sami Khan <120458750+Samidatabeys@users.noreply.github.com>
2024-03-12 16:52:54 +0500

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
 
     var divElement = document.getElementById("swell-customer-identification");
     var email = divElement.getAttribute("data-email");
+    console.log(email)
     var iframe = document.getElementById("iframe");
     iframe.onload = function () {
       iframe.contentWindow.postMessage(email, "*");
