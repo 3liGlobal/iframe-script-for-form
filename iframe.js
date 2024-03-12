cf422af66fd63fbb0a9ab292ca286485f1b0910b
Sami Khan <120458750+Samidatabeys@users.noreply.github.com>
2024-03-12 16:47:19 +0500

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,6 +1,16 @@
-var divElement = document.getElementById("swell-customer-identification");
-var email = divElement.getAttribute("data-email");
-var iframe = document.getElementById("iframe");
-iframe.onload = function () {
-  iframe.contentWindow.postMessage(email, "*");
-};
+$(document).ready(function () {
+  setTimeout(function () {
+    console.log("Howdy!");
+
+    var divElement = document.getElementById("swell-customer-identification");
+    var email = divElement.getAttribute("data-email");
+    var iframe = document.getElementById("iframe");
+    iframe.onload = function () {
+      iframe.contentWindow.postMessage(email, "*");
+    };
+
+    // var inputValue = $("#inputField").val();
+    // var iframe = document.getElementById("myFrame");
+    // iframe.contentWindow.postMessage(inputValue, "https://3liglobal.github.io");
+  }, 10000);
+});
