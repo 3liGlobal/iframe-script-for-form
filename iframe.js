5637934b4ae39d00bf631b9957faac1873b5414a
Sami Khan <120458750+Samidatabeys@users.noreply.github.com>
2024-03-12 18:55:44 +0500

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,19 +1,7 @@
-// $(document).ready(function () {
-//   setTimeout(function () {
-//     console.log("Howdy!");
-//     var divElement = document.getElementById("swell-customer-identification");
-//     var email = divElement.getAttribute("data-email");
-//     console.log(email);
-//     var iframe = document.getElementById("iframe");
-//     iframe.contentWindow.postMessage(email, "*");
-//   }, 10000);
-// });
 document.addEventListener("DOMContentLoaded", function() {
   setTimeout(function () {
-      console.log("Howdy!");
       var divElement = document.getElementById("swell-customer-identification");
       var email = divElement ? divElement.getAttribute("data-email") : null;
-      console.log(email);
       var iframe = document.getElementById("iframe");
       if (iframe) {
           var iframeWindow = iframe.contentWindow;
