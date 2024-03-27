7d4513ab6efd49e2d21badd3d51f0a496f3529c0
Sami Khan <120458750+Samidatabeys@users.noreply.github.com>
2024-03-27 14:00:24 +0500

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,17 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
-  setTimeout(function () {
-      var divElement = document.getElementById("swell-customer-identification");
-      var email = divElement ? divElement.getAttribute("data-email") : null;
-      var iframe = document.getElementById("iframe");
-      if (iframe) {
-          var iframeWindow = iframe.contentWindow;
-          if (iframeWindow) {
-              iframeWindow.postMessage(email, "*");
-          } else {
-              console.error("IFrame contentWindow is null.");
-          }
-      } else {
-          console.error("IFrame element not found.");
-      }
-  }, 15 * 1000);
+
+document.addEventListener("DOMContentLoaded", function () {
+    var checkEmailInterval = setInterval(function () {
+        var divElement = document.getElementById("swell-customer-identification");
+        var email = divElement ? divElement.getAttribute("data-email") : null;
+
+        if (email) { // If email is not null or empty
+            var iframe = document.getElementById("iframe");
+            if (iframe) {
+                var iframeWindow = iframe.contentWindow;
+                if (iframeWindow) {
+                    iframeWindow.postMessage(email, "*");
+                    clearInterval(checkEmailInterval); // Stop checking once the email is found and message is posted
+                } else {
+                    console.error("IFrame contentWindow is null.");
+                }
+            } else {
+                console.error("IFrame element not found.");
+            }
+        }
+    }, 1000); // Check every second
 });
